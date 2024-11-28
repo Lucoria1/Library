39557100035b90441bad4c1a07117fccc50764b2
Lucoria <183291283+Lucoria1@users.noreply.github.com>
2024-11-28 10:14:34 +0000

    created book constructor, not sure if it works

---
+++ main.js
@@ -0,0 +1,12 @@
+function createBook (title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return this.title,
+        this.author,
+        this.pages,
+        this.read
+    }
+}
