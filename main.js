aac02e1abca2e57bf9fd0c3442176738eb81fe18
Lucoria <183291283+Lucoria1@users.noreply.github.com>
2024-11-28 12:40:19 +0000

    added bookloop to display books and card styling

--- main.js
+++ main.js
@@ -12,19 +12,55 @@ function Book (title, author, pages, read){
             return `${this.title} by ${this.author}, ${this.pages} pages, has been read`
         } else {return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`}
     }
-}
+};
 
 function addBookToLibrary (book) {
     myLibrary.push(book)
-}
+};
 
-const book1 = new Book ("Guess How Much I Love You", "Sam McBratney", 32, true)
 
-addBookToLibrary (book1);
+function loopBooks () {
+    myLibrary.forEach((book, i)=>{
+        const div = document.createElement("div");
+        div.setAttribute("id", `bookCont ${i+1}`);
+        div.setAttribute("class", "bookCard");
+        document.getElementById("bookDisplay").appendChild(div);
+
+        const title = document.createElement("h3");
+        title.innerHTML = book.title;
+        document.getElementById(`bookCont ${i+1}`).appendChild(title);
+
+        const author = document.createElement("h4");
+        author.innerHTML = `Author: ${book.author}`;
+        document.getElementById(`bookCont ${i+1}`).appendChild(author);
+
+        const pages = document.createElement("h4");
+        pages.innerHTML = `Pages: ${book.pages}`;
+        document.getElementById(`bookCont ${i+1}`).appendChild(pages);
+        
+        const read = document.createElement("h4");
+        if(book.read === true){
+            read.innerHTML = `Read: has been read`} else {
+                read.innerHTML = `Read: has not been read`
+            };        
+        document.getElementById(`bookCont ${i+1}`).appendChild(read);
+    })
+};
+
+let book1 = new Book ("Guess How Much I Love You", "Sam McBratney", 32, true)
+
+addBookToLibrary(book1);
+
+let book2 = new Book ("The Very Hungry Caterpillar", "Eric Carle", 22, true)
+
+addBookToLibrary(book2);
 
 const addBookBtn = document.querySelector("#addBook")
 
+loopBooks();
+
 addBookBtn.addEventListener("click", (event) => {
     console.log("hello")
+
 })
 })
