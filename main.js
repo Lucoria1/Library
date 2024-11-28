f75d5e6922e322d666ac42ab45c98e860780e853
Lucoria <183291283+Lucoria1@users.noreply.github.com>
2024-11-28 11:32:25 +0000

    added a button and addbook/library functions

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-function createBook (title, author, pages, read){
+window.addEventListener("load", (event)=> {
+
+const myLibrary = [];
+
+function Book (title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function () {
-        return this.title,
-        this.author,
-        this.pages,
-        this.read
+    this.info = function() {
+        if(this.read === true){
+            return `${this.title} by ${this.author}, ${this.pages} pages, has been read`
+        } else {return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`}
     }
 }
+
+function addBookToLibrary (book) {
+    myLibrary.push(book)
+}
+
+const book1 = new Book ("Guess How Much I Love You", "Sam McBratney", 32, true)
+
+addBookToLibrary (book1);
+
+const addBookBtn = document.querySelector("#addBook")
+
+addBookBtn.addEventListener("click", (event) => {
+    console.log("hello")
+})
+})
